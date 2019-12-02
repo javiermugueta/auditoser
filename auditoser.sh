77c3727ade08c97934db6bf024a2f59783711f98
javi <javiermugueta@gmail.com>
2019-12-02 16:20:39 +0100

    .

--- auditoser.sh
+++ auditoser.sh
@@ -25,12 +25,12 @@ if echo "$os" | grep -q "Linux"; then
     # linuxlike
     thedayafterday=$(date --date='-1 day' "+%Y-%m-%d")
     initial="${thedayafterday}T00:00:00.000Z"
-    final="${thedayafterday}T23:59:59.999Z";
+    final="${thedayafterday}T00:00:59.999Z";
 elif echo "$os" | grep -q "Darwin"; then
     # mac
     thedayafterday=$(date -v-1d "+%Y-%m-%d")
     initial="${thedayafterday}T00:00:00.000Z"
-    final="${thedayafterday}T00:00:59.999Z"
+    final="${thedayafterday}T23:59:59.999Z"
 else
     #
     echo "I haven't been instructed by the programmer how to deal with date"
